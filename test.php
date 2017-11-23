59fad4b7725072d56efd7e439f3f8ac96b692d3e
keloriane <kevin.flbt@gmail.com>
2017-11-23 16:29:16 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,25 @@
 <?php
-
-    if (isset($_GET["nom"])&&isset($_GET["mail"])&&isset($_GET["prenom"])) {
-    
-         
-      $bdd = new PDO('mysql:host=localhost;dbname=ubeer;charset=utf8','root','');
-      $envoi = $bdd->prepare('INSERT INTO subscriber(nom,prenom,mail,cocher)VALUES (:nom, :prenom , :mail, 0)');
-      $envoi->execute(array(
-        'nom' => $_GET["nom"],
-        'prenom' => $_GET["prenom"],
-        'mail' => $_GET["mail"]  
-      
-      ));
-    }
-?>
+try
+{
+    // On se connecte à MySQL
+    $pdo = new PDO('pgsql:host=ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;dbname=doim6sdhvcmlf;', 'trsngdqkrvpaeq', '8753949bc68a35626b907fe0d0e0a818626905874e40d6a02fb248bf038db27f');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+$query = 'INSERT INTO categories (nom, description) VALUES (?, ?);';
+$prep = $pdo->prepare($query);
+ 
+$prep->bindValue(1, 'bertand', PDO::PARAM_STR);
+$prep->bindValue(2, 'ceci est un test pour desc', PDO::PARAM_STR);
+$prep->execute();
+$resultat = $pdo->query('SELECT * FROM categories');
+while ($donnees = $resultat->fetch())
+{
+  echo '<br/>';
+  echo $donnees['nom'];
+  echo ' : ';
+  echo $donnees['description'];
+}
